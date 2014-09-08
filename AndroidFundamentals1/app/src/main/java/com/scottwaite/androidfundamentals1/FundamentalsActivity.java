aa4511c86d09b7fe4b6ad88223d8c0a7ea24885d
scottwaite <scottwaite@Scotts-MacBook-Pro.local>
2014-09-07 20:50:42 -0400

    Added Events and Listeners

--- AndroidFundamentals1/app/src/main/java/com/scottwaite/androidfundamentals1/FundamentalsActivity.java
+++ AndroidFundamentals1/app/src/main/java/com/scottwaite/androidfundamentals1/FundamentalsActivity.java
@@ -1,21 +1,61 @@
 package com.scottwaite.androidfundamentals1;
 
-// Created By: Scott Waite
-// September 7, 2014
+/*
+Created By: Scott Waite
+Course: Java 1
+Instructor: Gyasi Story
+Assignment: Mastering the Fundamentals
+Date: September 7, 2014
+*/
 
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Button;
 
 
 public class FundamentalsActivity extends Activity {
+
+    //Debugging tag
+    final String TAG = "Android Fundamentals1";
+    private TextView mUserText;
+    private TextView mResultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundamentals);
+
+        //Setting references to user and result text fields
+        mUserText = (TextView) findViewById(R.id.usertext);
+        mResultText = (TextView) findViewById(R.id.resulttext);
+
+        Button button = (Button) findViewById(R.id.userbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                mResultText.setText(mUserText.getText());
+            }
+        });
+
+        ListView oslist = (ListView) findViewById(R.id.oslist);
+        oslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "List Item Selected");
+            }
+
+        });
+
+
     }
 
 
