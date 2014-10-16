a023c8927af6be7c6004673e1d4e63812f8e91fe
scottwaite <scottwaite@macbookpro-cb99.home>
2014-10-16 19:09:33 -0400

    App with Spinner

---
+++ Week 3/MyApplication/app/src/main/java/com/scottwaite/myapplication/CustomOnItemSelectedListener.java
@@ -0,0 +1,27 @@
+package com.scottwaite.myapplication;
+
+/**
+ * Created by scottwaite on 10/16/14.
+ */
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,
+                               long id) {
+        Toast.makeText(parent.getContext(),
+                "Flavor Selection: " + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
--- Week 3/MyApplication/app/src/main/java/com/scottwaite/myapplication/MyActivity.java
+++ Week 3/MyApplication/app/src/main/java/com/scottwaite/myapplication/MyActivity.java
@@ -1,36 +1,81 @@
 package com.scottwaite.myapplication;
+/*
+Scott Waite
+Assignment: Working with Advanced Views
+Instructor: Gyasi Story
+Date: 10/16/14
+ */
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MyActivity extends Activity {
 
+    private Spinner spinner1, spinner2;
+    private Button submitButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        addItemsOnSpinner2();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
     }
 
+    // Add selections to spinner
+    public void addItemsOnSpinner2() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("Small cone");
+        list.add("Medium cone");
+        list.add("Large cone");
+        list.add("Dish");
+        list.add("Sundae");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void addListenerOnSpinnerItemSelection(){
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
+
+    // Get the selection from the dropdown
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MyActivity.this,
+                        "Order Summary: " +
+                                "\nFlavor: " + String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSize: " + String.valueOf(spinner2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
 }
