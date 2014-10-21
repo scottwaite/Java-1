9036d09a4abdfde818bac697c858d3356aceabb1
scottwaite <scottwaite@MACBOOKPRO-CB99.home>
2014-10-21 16:57:32 -0400

    Week 4 Initial Commit

---
+++ Week 4/MyApplication/app/src/main/java/com/scottwaite/myapplication/MyActivity.java
@@ -0,0 +1,82 @@
+package com.scottwaite.myapplication;
+/*
+Scott Waite
+Assignment: A Connected Application
+Instructor: Gyasi Story
+Date: 10/21/14
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity{
+
+    private Spinner spinner1, spinner2;
+    private Button submitButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        addItemsOnSpinner2();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+    }
+
+    // Add selections to spinner
+    public void addItemsOnSpinner2() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("Small cone");
+        list.add("Medium cone");
+        list.add("Large cone");
+        list.add("Dish");
+        list.add("Sundae");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+    }
+
+    public void addListenerOnSpinnerItemSelection(){
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    // Get the selection from the dropdown
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MyActivity.this,
+                        "Order Summary: " +
+                                "\nFlavor: " + String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSize: " + String.valueOf(spinner2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+}
